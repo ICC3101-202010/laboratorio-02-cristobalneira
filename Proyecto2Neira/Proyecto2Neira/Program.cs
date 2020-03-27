509700a423e1f58a35d8a177cf034138b927aaed
Cristobal <c@iMac-de-Rene.local>
2020-03-27 15:09:57 -0300

    First commit

---
+++ Proyecto2Neira/Proyecto2Neira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto2Neira
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
